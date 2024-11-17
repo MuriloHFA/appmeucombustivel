eb82a6b0f9cbe1ecbc947ab9a189bdfc0931aec7
muril <muril@DESKTOP-PP4GOHK>
2024-11-17 16:01:48 -0300

    Adicionar arquivos de projeto.

---
+++ appmeucombustivel/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace appmeucombustivel
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+
+                double etanol = Convert.ToDouble(txt_etanol.Text);
+                double gasolina = Convert.ToDouble(txt_gasolina.Text);
+
+                string msg = "";
+
+                if (etanol <= (gasolina * 0.7) )
+                {
+                    msg = "O etanol está compensado.";
+                } else
+                {
+                    msg = "A gasolina está compensado.";
+                }
+
+                DisplayAlert("Calculado", msg, "OK");
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "Fechar");
+            }
+
+        } //fecha metodo
+    } //fecha classe
+
+} //fecha namespace
